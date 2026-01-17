1f282fcfc5b1dac72dc5f0f5369e06da0b4c90ce
DevWave <devwave@outlook.de>
2026-01-17 13:02:41 +0100

    feat(day1): complete JS variables exercises

--- learning-path/phase-1-frontend/week-2-javascript-basics/day-1-variables/script.js
+++ learning-path/phase-1-frontend/week-2-javascript-basics/day-1-variables/script.js
@@ -1,10 +1,43 @@
 // Day 1 - Variables
 
-// TODO: declare variables here
+// String variable
+let firstName = 'DevWave';
 
-// Example
-let name = 'DevWave';
-const year = 2026;
+// Number variable
+let age = 26;
 
-console.log('Name:', name);
-console.log('Year:', year);
+// Boolean variable
+const isDeveloper = true;
+
+// Print to console
+console.log('Name:', firstName);
+console.log('Age:', age);
+console.log('Is Developer:', isDeveloper);
+
+// TASK 2
+
+// Reassign let variable
+firstName = 'CodeMaster';
+age = 30;
+
+//Attempt to reassign const variable (this will throw an error)
+
+try {
+  isDeveloper = false;
+} catch (error) {
+  console.log('Cannot reassign a const variable!');
+}
+
+console.log('Updated Name:', firstName);
+console.log('Updated Age:', age);
+
+// TASK 3
+
+// Concatenate string
+let greeting = 'Hello, ' + firstName + '!';
+
+// Template literals (modern JS)
+let message = `Hello, ${firstName}! You are ${age} years old.`;
+
+console.log(greeting);
+console.log(message);
