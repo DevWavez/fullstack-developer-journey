941e3c2264dc42e04690560a02669075a37a678c
DevWave <devwave@outlook.de>
2026-01-18 12:08:46 +0100

    feat(day4) add conditionals exercises

---
+++ learning-path/phase-1-frontend/week-2-javascript-basics/day-4-conditionals/README.md
@@ -0,0 +1,17 @@
+# Day 4 – Conditionals (if / else)
+
+## Topics
+
+- if / else
+- comparison operators
+- logical thinking
+
+## Goal
+
+Learn how JavaScript makes decisions based on conditions.
+
+## Key Learnings
+
+- Code can react to different situations
+- Conditions control program flow
+- Logic is more important than syntax
---
+++ learning-path/phase-1-frontend/week-2-javascript-basics/day-4-conditionals/script.js
@@ -0,0 +1,39 @@
+// Task 1: simple if/else statement
+let age = 18;
+if (age >= 18) {
+  console.log('You are an adult.');
+} else {
+  console.log('You are a minor.');
+}
+
+// Task 2: Boolean condition
+const isLoggedIn = true;
+if (isLoogedIn) {
+  console.log('Welcome back!');
+} else {
+  console.log('Please log in.');
+}
+
+// Task 3: Multiple conditions with else if
+const score = 75;
+if (score >= 90) {
+  console.log('Grade: A');
+} else if (score >= 80) {
+  console.log('Grade: B');
+} else if (score >= 70) {
+  console.log('Grade: C');
+} else if (score >= 60) {
+  console.log('Grade: D');
+} else {
+  console.log('Grade: F');
+}
+
+// Mini Challenge: Multiple condition with else if
+const temperature = 15;
+if (temperature > 20) {
+  console.log("It's warm outside.");
+} else if (temperature >= 0 && temperature <= 20) {
+  console.log("It's cool outside.");
+} else {
+  console.log("It's cold outside.");
+}
